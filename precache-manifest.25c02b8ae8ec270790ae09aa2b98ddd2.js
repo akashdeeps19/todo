a2a730b450aaba1a6f39a4151331f67f8a7a9bec
Akashdeep-S9 <akasdeeps19@gmail.com>
2019-12-13 08:58:32 +0530

    Updates

---
+++ precache-manifest.25c02b8ae8ec270790ae09aa2b98ddd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d484c1af37d19bccf16668462d3e39ff",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "2bd46a93842c883132ed",
+    "url": "/todo/static/css/main.663f13b8.chunk.css"
+  },
+  {
+    "revision": "1698901fab6ce2462807",
+    "url": "/todo/static/js/2.dea73cfe.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/todo/static/js/2.dea73cfe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2bd46a93842c883132ed",
+    "url": "/todo/static/js/main.c9fa3984.chunk.js"
+  },
+  {
+    "revision": "ed044effc7b6bca18d34",
+    "url": "/todo/static/js/runtime-main.334cb69f.js"
+  }
+]);
